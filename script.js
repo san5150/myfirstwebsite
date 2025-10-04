321320ea33f434cdcf5c826db06d8e508f70a31a
san5150 <90774856+san5150@users.noreply.github.com>
2025-10-04 11:16:22 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Scroll to features on button click
+document.getElementById("learnMoreBtn").addEventListener("click", function() {
+  document.getElementById("features").scrollIntoView({ behavior: "smooth" });
+});
+
+// Mobile nav toggle (optional)
